a124bcbc6cf944a4063d920ef068537db1ec80bd
bugsywap <ollerogabe@gmail.com>
2023-12-12 15:03:53 +0800

    Final Project CIT17

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+include 'setup.php';
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Student Information System</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">Final Project - OLLERO</a>
+            <a href="displayUser.php" class="list-group-item list-group-item-action list-group-item-primary">Users</a>
+            <a href="displayStudent.php" class="list-group-item list-group-item-action list-group-item-primary">Students</a>
+            <a href="displayInstructor.php" class="list-group-item list-group-item-action list-group-item-primary">Instructors</a>
+            <a href="displayCourse.php" class="list-group-item list-group-item-action list-group-item-primary">Courses</a>
+        </div>
+    </div>
+    <h2 class="container my -5">Gabriel Albert P. Ollero</h2>
+    <h3 class="container my -5">CIT 17 - Web Information System</h3>
+
+</body>
+
+</html>
